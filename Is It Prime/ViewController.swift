63c6f2d678f9b6d421fdee86e42b2c8b3d49d12d
Juan Ramirez <juanramirez@mail.usf.edu>
2017-01-19 12:55:51 -0500

    first commit

---
+++ Is It Prime/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Is It Prime
+//
+//  Created by JUAN RAMIREZ on 1/19/17.
+//  Copyright © 2017 EZ IT Apps, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputAnswer: UILabel!
+    
+    @IBOutlet weak var inputAnswer: UITextField!
+    
+    @IBAction func checkPrimeButton(_ sender: Any) {
+        
+        // Check if there is a value in the text field
+        if inputAnswer.text != nil{
+            
+            if let checkIfNumber = Int(inputAnswer!.text!) {
+                
+                
+                outputAnswer.text = checkForPrime(checkIfNumber: checkIfNumber)
+                
+                
+            } else {
+                outputAnswer.text = "Value must be a whole number"
+            }
+        } else {
+            outputAnswer.text = "Please input a value"
+        }
+    }
+    
+    func checkForPrime(checkIfNumber:Int) -> String {
+        var interval = 2
+        
+        if checkIfNumber == 1 {
+            return "\(checkIfNumber) is an exception"
+        }
+        
+        while interval < checkIfNumber {
+            if checkIfNumber % interval == 0 {
+                return "\(checkIfNumber) is not a prime!"
+            }
+            interval += 1
+        }
+        return "\(checkIfNumber) is a prime!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
